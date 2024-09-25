8783d5c28861cf4cd48e394eaffa09cf802dc10f
Thaís <thaiscastro95@yahoo.com>
2024-09-25 20:37:51 -0300

    Add files via upload

---
+++ index2.js
@@ -0,0 +1,20 @@
+let i=1
+let num=0
+let mult=0
+
+
+
+for(i=1;i<=9;i++){
+
+    for(num=0;num<=10;num++){
+
+
+        mult=i*num
+
+        console.log(`${i} X ${num} = ${mult}`)
+
+    }
+
+}
+
+
---
+++ index4.js
@@ -0,0 +1,12 @@
+let num = Number (prompt("Digite um numero "))
+let i
+let mult=1
+
+for(i=1; i<=num; i++){
+
+
+    mult=mult *i 
+    
+}
+
+console.log(mult)
